6ce3bd183017b4b0453b6a237dce56b3f0ec6bc2
Rafał Misiarek <misiarek@gmail.com>
2021-08-15 17:24:32 +0200

    Update settings for consumer services (#4)

--- settings.py
+++ settings.py
@@ -2,9 +2,16 @@
 
 BROKER = "127.0.0.1:9092"
 
-CONSUMER_CONFIG = {
+RECOMMENDATION_SERVICE_CONFIG = {
     'bootstrap.servers': BROKER,
-    'group.id': "test-group",
+    'group.id': "recommendation-group",
+    'session.timeout.ms': 6000,
+    'auto.offset.reset': 'latest',
+}
+
+DASHBOARD_SERVICE_CONFIG = {
+    'bootstrap.servers': BROKER,
+    'group.id': "dashboard-group",
     'session.timeout.ms': 6000,
     'auto.offset.reset': 'latest',
 }
